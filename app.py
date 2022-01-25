571a244a7e54c50f84b0e8914a0d186ee0f0c9ba
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 07:28:16 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import joblib
-st.title('Spam Ham Deployment')
-test_model=joblib.load('spam_ham')
-ip=st.text_input('Enter your message')
-op=test_model.predict([ip])
-if st.button('Predict'):
-  st.title(op)
