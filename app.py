7eaef2e748f58992ab3a82816edaa7ed7206294f
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 07:28:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+st.title('Spam Ham Deployment')
+test_model=joblib.load('sentiment_analysis')
+ip=st.text_input('Enter your message')
+op=test_model.predict([ip])
+if st.button('Predict'):
+  st.title(op)
